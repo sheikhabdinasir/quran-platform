8d35f7584fb07c402da6f93cf3115bb8a70a3aa7
hamzemursal2 <hamzemursal2@gmail.com>
2026-05-20 00:07:35 +0530

    Fixed Gmail timeout issue

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,20 +1,45 @@
+```js id="jlwmu0"
 import nodemailer from "nodemailer";
 
-const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
+const sendEmail = async (
+  to,
+  subject,
+  text
+) => {
+
+  const transporter =
+  nodemailer.createTransport({
+
     service: "gmail",
+
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user:
+      process.env.EMAIL_USER,
+
+      pass:
+      process.env.EMAIL_PASS,
     },
+
+    tls: {
+      rejectUnauthorized:false,
+    },
+
   });
 
   await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+
+    from:
+    process.env.EMAIL_USER,
+
     to,
+
     subject,
+
     text,
+
   });
+
 };
 
 export default sendEmail;
+```
