97b48608737d6bff83266ffe97909a8cca7830ce
hamzemursal2 <hamzemursal2@gmail.com>
2026-05-25 11:23:27 +0530

    Updated Gmail app password

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,20 +1,45 @@
 import nodemailer from "nodemailer";
 
-const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const sendEmail = async (
+  to,
+  subject,
+  text
+) => {
+
+  const transporter =
+    nodemailer.createTransport({
+
+      host: "smtp.gmail.com",
+
+      port: 587,
+
+      secure: false,
+
+      auth: {
+
+        user:
+          process.env.EMAIL_USER,
+
+        pass:
+          process.env.EMAIL_PASS,
+
+      },
+
+    });
 
   await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+
+    from:
+      process.env.EMAIL_USER,
+
     to,
+
     subject,
+
     text,
+
   });
+
 };
 
 export default sendEmail;
