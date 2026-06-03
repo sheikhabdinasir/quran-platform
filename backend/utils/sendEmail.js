5fc2c6bb1c2dcbb66f446ce29ea634316aa3dac1
hamzemursal2 <hamzemursal2@gmail.com>
2026-06-03 22:59:29 +0530

    debug forgot password

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,7 +2,9 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: process.env.EMAIL_HOST,
+    port: Number(process.env.EMAIL_PORT),
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
