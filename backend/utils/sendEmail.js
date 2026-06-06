9ddff66466e5b86193939b8e583f9ef952b18ac6
hamzemursal2 <hamzemursal2@gmail.com>
2026-06-07 00:46:05 +0530

    install resend backend

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,6 +2,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+
 const sendEmail = async (to, subject, text, html) => {
   const { data, error } = await resend.emails.send({
     from: "Tafsiir Platform <onboarding@resend.dev>",
