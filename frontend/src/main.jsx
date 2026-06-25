256219fa443e7f6c1230d8ac9e028d029e90953f
hamzemursal2 <hamzemursal2@gmail.com>
2026-06-25 13:58:47 +0530

    ui faq

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import "./styles.css";
+import "../Navbar.css";
 
 /* EXISTING */
 import { PublicProvider } from "./Context/PublicContext.jsx";
