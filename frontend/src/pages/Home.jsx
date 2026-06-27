41fcb2f73b501ea891a5fa65383206dba64012b1
hamzemursal2 <hamzemursal2@gmail.com>
2026-06-27 13:43:38 +0530

    home css app

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+
 import FAQ from "../components/FAQ";
 import "./Home.css";
 import QuickActions from "../components/QuickActions";
